2abddb4927401adc60aeb7e55268fab4b9e7ab8e
RafiulRifat <rafiulsiddique301@gmail.com>
2024-06-23 11:34:03 +0600

    Feature-4

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,21 @@
-import Header from './components/Header/Header'
-import Hero from './components/Hero/Hero';
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 import "./App.css";
-import Companies from './components/companies/companies';
-import Residencies from './components/Residencies/Residencies';
-import Value from './components/Value/Value';
+import Companies from "./components/companies/companies";
+import Residencies from "./components/Residencies/Residencies";
+import Value from "./components/Value/Value";
 
 function App() {
   return (
     <div className="App">
       <div>
-        <div className="white-gradient"/>
-
-        
-
-      
-      
-      <Header/>
-      <Hero/>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
       </div>
-      <Companies/>
-      <Residencies/>
-      <Value/>
-     
-      
-     
+      <Companies />
+      <Residencies />
+      <Value />
     </div>
   );
 }
